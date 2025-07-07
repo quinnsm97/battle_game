38f358911921add7f76ce8a730b03b8c16195b77
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-07-07 22:44:55 +1000

    Add critical strike chance to attacks

--- game/actions.py
+++ game/actions.py
@@ -1,5 +1,11 @@
+import random
+
 def attack(attacker, defender):
     damage = attacker.attack
+    #critical strike logic
+    if random.randint(1, 10) == 1:
+        print("Critical strike!")
+        damage *= 2
     return defender.take_damage(damage)
 
 def defend(player):
